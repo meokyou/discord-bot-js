62aaf056027f8404e80f13190022aeeac32074c1
Christian Anderson <christian.n.anderson@protonmail.com>
2022-12-22 16:48:33 -0600

    Correct verification

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+		.setDMPermission(true),
 	async execute(interaction) {
 		return interaction.reply('Pong!');
 	},
